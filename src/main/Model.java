94b13cd2261874d3aacd08805889110c377b731d
Ausias Martinez <aumabe@alumnatflorida.es>
2024-10-02 15:41:38 +0200

    Changed variable names to adjust to language requirements

--- src/main/Model.java
+++ src/main/Model.java
@@ -4,36 +4,36 @@ import java.io.File;
 import java.text.SimpleDateFormat;
 
 public class Model {
-	
+
 	public String getEstructuraFitxers(File rutaDir) {
-		return rutaDir.getName() + "\r\n" + imprimeEstructura(rutaDir, 0);
+		return rutaDir.getName() + "\r\n" + imprimixEstructura(rutaDir, 0);
 	}
-	
-	private static String imprimeEspacios(int subNivel) {
+
+	private static String imprimixEspais(int subNivell) {
 		String marca = "|-- ";
-		for (int i = 0; i < subNivel; i++) {
+		for (int i = 0; i < subNivell; i++) {
 			marca = "|   " + marca;
 		}
 		return marca;
 	}
 
-	private static String imprimeEstructura(File dir, int subNivel) {
+	private static String imprimixEstructura(File dir, int subNivell) {
 		SimpleDateFormat df = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
-		File[] subDirectorios = dir.listFiles();
+		File[] subDirectoris = dir.listFiles();
 		String estructura = "";
-		for (int i = 0; i < subDirectorios.length; i++) {
-			if (subDirectorios[i].isDirectory()) {
-				if (subDirectorios[i].list().length != 0) {
-					estructura += imprimeEspacios(subNivel) + "\\" + subDirectorios[i].getName() + "\r\n";
-					estructura += imprimeEstructura(subDirectorios[i], subNivel + 1);
+		for (int i = 0; i < subDirectoris.length; i++) {
+			if (subDirectoris[i].isDirectory()) {
+				if (subDirectoris[i].list().length != 0) {
+					estructura += imprimixEspais(subNivell) + "\\" + subDirectoris[i].getName() + "\r\n";
+					estructura += imprimixEstructura(subDirectoris[i], subNivell + 1);
 				}
 			} else {
-				estructura += imprimeEspacios(subNivel) + subDirectorios[i].getName() + " ("
-						+ (Math.round(subDirectorios[i].length() / 1024.0f * 10.0f) / 10.0f) + " KB - "
-						+ df.format(subDirectorios[i].lastModified()) + ")" + "\r\n";
+				estructura += imprimixEspais(subNivell) + subDirectoris[i].getName() + " ("
+						+ (Math.round(subDirectoris[i].length() / 1024.0f * 10.0f) / 10.0f) + " KB - "
+						+ df.format(subDirectoris[i].lastModified()) + ")" + "\r\n";
 			}
 		}
 		return estructura;
 	}
-	
+
 }
